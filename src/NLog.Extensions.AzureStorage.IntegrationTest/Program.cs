73d686f13e63623d5795ca37c75a20c4ab568c93
Justin Detmar <JDetmar@users.noreply.github.com>
2017-02-21 10:54:24 -0800

    bug fixed on log message layout

--- src/NLog.Extensions.AzureStorage.IntegrationTest/Program.cs
+++ src/NLog.Extensions.AzureStorage.IntegrationTest/Program.cs
@@ -13,18 +13,18 @@ namespace NLog.Extensions.AzureStorage.IntegrationTest
         static void Main(string[] args)
         {
             var logger = LogManager.GetCurrentClassLogger();
-
-            for (int i = 0; i < 100; i++)
-            {
-                logger.Trace("Trace Message: " + i);
-                logger.Debug("Debug Message: " + i);
-                logger.Info("Info Message: " + i);
-                logger.Warn("Warn Message: " + i);
-                logger.Error("Error Message: " + i);
-                logger.Fatal("Fata Message: " + i);
-                Thread.Sleep(10);
-            }
-
+            logger.Trace("Log this");
+//            for (int i = 0; i < 100; i++)
+//            {
+//                logger.Trace("Trace Message: " + i);
+//                logger.Debug("Debug Message: " + i);
+//                logger.Info("Info Message: " + i);
+//                logger.Warn("Warn Message: " + i);
+//                logger.Error("Error Message: " + i);
+//                logger.Fatal("Fata Message: " + i);
+//                Thread.Sleep(10);
+//            }
+//
             try
             {
                 throw new NotImplementedException();
