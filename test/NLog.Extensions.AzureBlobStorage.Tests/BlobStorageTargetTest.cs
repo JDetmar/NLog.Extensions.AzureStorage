2b40b330c31a2b545050569ce83d34bee93b18b0
Rolf Kristensen <sweaty1@hotmail.com>
2021-02-15 16:09:45 +0100

    BlobStorageTarget - Added support for roll when using cached=true in BlobName

--- test/NLog.Extensions.AzureBlobStorage.Tests/BlobStorageTargetTest.cs
+++ test/NLog.Extensions.AzureBlobStorage.Tests/BlobStorageTargetTest.cs
@@ -56,8 +56,45 @@ namespace NLog.Extensions.AzureBlobStorage.Tests
             }
             logFactory.Flush();
             Assert.Equal(2, cloudBlobService.AppendBlob.Count);   // Two partitions
-            Assert.NotEqual(string.Empty, cloudBlobService.PeekLastAppendBlob("info", "Test1"));
-            Assert.NotEqual(string.Empty, cloudBlobService.PeekLastAppendBlob("debug", "Test2"));
+            Assert.NotNull(cloudBlobService.PeekLastAppendBlob("info", "Test1"));
+            Assert.NotNull(cloudBlobService.PeekLastAppendBlob("debug", "Test2"));
+        }
+
+        [Fact]
+        public void RollOnBlockCountExceedsLimit()
+        {
+            var logFactory = new LogFactory();
+            var logConfig = new Config.LoggingConfiguration(logFactory);
+            logConfig.Variables["ConnectionString"] = nameof(BlobStorageTargetTest);
+            var cloudBlobService = new CloudBlobServiceMock();
+            var logEventInfo = LogEventInfo.CreateNullEvent();
+            var blobStorageTarget = new BlobStorageTarget(cloudBlobService);
+            blobStorageTarget.ConnectionString = "${var:ConnectionString}";
+            blobStorageTarget.Container = "${level}";
+            blobStorageTarget.BlobName = "${logger}_${sequenceid:cached=true}";
+            blobStorageTarget.Layout = "${message}";
+            logConfig.AddRuleForAllLevels(blobStorageTarget);
+            logFactory.Configuration = logConfig;
+
+            var logEvent1 = LogEventInfo.Create(LogLevel.Info, null, "Hello");
+            logFactory.GetLogger("Test1").Log(logEvent1);
+            logFactory.Flush();
+            Assert.Contains("Hello", cloudBlobService.PeekLastAppendBlob("info", "Test1_" + logEvent1.SequenceID.ToString()));
+            var logEvent2 = LogEventInfo.Create(LogLevel.Info, null, "Goodbye");
+            logFactory.GetLogger("Test1").Log(logEvent2);
+            logFactory.Flush();
+            Assert.Contains("Goodbye", cloudBlobService.PeekLastAppendBlob("info", "Test1_" + logEvent1.SequenceID.ToString()));
+
+            cloudBlobService.FirstBlobNameHasBlockCountExceeded = true;
+
+            var logEvent3 = LogEventInfo.Create(LogLevel.Info, null, "Hello Roll");
+            logFactory.GetLogger("Test1").Log(logEvent3);
+            logFactory.Flush();
+            Assert.Contains("Hello Roll", cloudBlobService.PeekLastAppendBlob("info", "Test1_" + logEvent3.SequenceID.ToString()));
+            var logEvent4 = LogEventInfo.Create(LogLevel.Info, null, "Goodbye Roll");
+            logFactory.GetLogger("Test1").Log(logEvent4);
+            logFactory.Flush();
+            Assert.Contains("Goodbye Roll", cloudBlobService.PeekLastAppendBlob("info", "Test1_" + logEvent3.SequenceID.ToString()));
         }
     }
 }
